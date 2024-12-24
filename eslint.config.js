292021b63fd6933929827bb128fc77070c6966a1
CreepyMemes <mjouins@gmail.com>
2024-12-24 12:57:25 +0100

    added react hooks and refresh eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import eslint from '@eslint/js';
 import globals from 'globals';
 
-import prettier from 'eslint-plugin-prettier/recommended';
+import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
@@ -22,16 +24,28 @@ export default [
       },
     },
 
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    settings: { react: { version: '18.3' } },
+
+    // Used plugins
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+
+    // ESLint rules configuration
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      ...prettier.configs.recommended.rules,
     },
   },
-
-  eslint.configs.recommended,
-  react.configs.flat.recommended,
-  react.configs.flat['jsx-runtime'],
-
-  prettier,
 ];
