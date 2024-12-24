4eae4c44f9be92976e46c1977b66273d24fa1991
CreepyMemes <mjouins@gmail.com>
2024-12-24 14:14:41 +0100

    changed rule no unused vars to warning

--- eslint.config.js
+++ eslint.config.js
@@ -42,6 +42,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
 
+      'no-unused-vars': 'warn',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 
